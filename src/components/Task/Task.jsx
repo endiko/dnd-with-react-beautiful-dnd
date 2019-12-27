10a783a4502120128d263095461beead92a8e9a6
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2019-12-27 11:46:30 +0500

    Add draghandleprops to Handle component with icon

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Draggable } from 'react-beautiful-dnd'
 import styled from 'styled-components'
+import { FaGripLines } from 'react-icons/fa'
 
 const Container = styled.div`
     border: 1px solid lightgrey;
     border-radius: 2px;
-    padding: 8px;
+    padding: 1rem 2rem;
     margin-bottom: 8px;
     background-color: #fff;
     box-shadow: ${props => (props.isDragging ? '0 0 5px 0 rgba(0,0,0,.3)' : 'none')};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Handle = styled.div`
+    display: inline-block;
 `;
 
 function Task(props) {
@@ -17,11 +25,14 @@ function Task(props) {
             {(provided, snapshot) => (
                 <Container
                     {...provided.draggableProps}
-                    {...provided.dragHandleProps}
+
                     ref={provided.innerRef}
                     isDragging={snapshot.isDragging}
                 >
                     {props.task.content}
+                    <Handle {...provided.dragHandleProps}>
+                        <FaGripLines style={{ width: '20px', height: '25px', fill: '#222' }} />
+                    </Handle>
                 </Container>
             )}
 
